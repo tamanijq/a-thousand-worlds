9b8b77ce711df2a07216cc9d0f2161db073f30de
Raine Revere <3917322+tamanijq@users.noreply.github.com>
2020-10-18 15:09:18 -0600

    Gruntfile: Minor formatting.

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,24 +12,27 @@ module.exports = function(grunt) {
     "markdown",
     "archieml",
     "shelve",
-    "validate"
+    "validate",
   ]);
   grunt.registerTask("template", "Build HTML from content/templates", [
     "content",
-    "build"
+    "build",
   ]);
   grunt.registerTask("static", "Build all files", [
     "copy",
     "bundle",
     "less",
-    "template"
+    "template",
   ]);
   grunt.registerTask("default", ["clean", "static", "connect:dev", "watch"]);
   grunt.registerTask("quick", "Build without assets", [
     "clean",
     "bundle",
     "less",
-    "template"
+    "template",
   ]);
-  grunt.registerTask("unclean", "Start dev server without a clean build", ["connect:dev", "watch"])
+  grunt.registerTask("unclean", "Start dev server without a clean build", [
+    "connect:dev",
+    "watch",
+  ])
 };
